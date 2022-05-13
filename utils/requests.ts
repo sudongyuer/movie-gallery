d4275731b41a1339690d45005c197abfe99554ba
sudongyu <976499226@qq.com>
2022-05-13 10:45:16 +0800

    feat: add request.ts

---
+++ utils/requests.ts
@@ -0,0 +1,49 @@
+// Typically we would store in {process.env.API_KEY}
+const API_KEY = 'ce134f48fbe38d5a8d3133ec8cc19eb2'
+console.log(API_KEY);
+export default {
+  fetchTrending: {
+    title: "Trending",
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchTopRated: {
+    title: "Top Rated",
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchActionMovies: {
+    title: "Action",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  },
+  fetchComedyMovies: {
+    title: "Comedy",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  },
+  fetchHorrorMovies: {
+    title: "Horror",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  },
+  fetchRomanceMovies: {
+    title: "Romance",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  },
+  fetchMystery: {
+    title: "Mystery",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+  },
+  fetchSciFi: {
+    title: "Sci-Fi",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  },
+  fetchWestern: {
+    title: "Western",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+  },
+  fetchAnimation: {
+    title: "Animation",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+  },
+  fetchTV: {
+    title: "TV Movie",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+  },
+};
