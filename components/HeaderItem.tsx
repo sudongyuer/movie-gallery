fbd2b64e66b704fe5554ef09655f9b12e73ef819
sudongyu <976499226@qq.com>
2022-05-13 10:44:25 +0800

    chore: update HeaderItem.ts

--- components/HeaderItem.tsx
+++ components/HeaderItem.tsx
@@ -3,7 +3,7 @@ function HeaderItem({title,Icon}:{title:string,Icon:(props: React.ComponentProps
   return (
     <div className='flex flex-col items-center w-12 sm:w-20 cursor-pointer  group  hover:text-white'>
       <Icon className='h-8 mb-1 group-hover:animate-bounce'/>
-      <p className='opacity-100  group-hover:opacity-100  tracking-widest'>{title}</p>
+      <p className='opacity-0  group-hover:opacity-100  tracking-widest'>{title}</p>
     </div>
   )
 }
