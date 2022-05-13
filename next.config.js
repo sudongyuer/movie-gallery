ef03a48bd557963eb38a2f4498110a2a9bff63a9
sudongyu <976499226@qq.com>
2022-05-13 22:38:38 +0800

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains:["press.hulu.com","links.papareact.com", "image.tmdb.org"]
+  },
+  env:{
+    API_KEY : process.env.API_KEY
   }
 }
 
