35569c7bfa8b18acb08cdaa1cc3714c9b7240c94
sudongyu <976499226@qq.com>
2022-05-14 00:18:48 +0800

    chore: update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: [],
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      },
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 }
