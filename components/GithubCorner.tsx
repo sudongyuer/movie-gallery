b2f12f171ea7cd19f36ae59bfa9afbd672f01b5e
sudongyu <976499226@qq.com>
2022-05-14 20:37:54 +0800

    feat: add GithubCorner.tsx

---
+++ components/GithubCorner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Github from 'react-github-corner';
+function GithubCorner() {
+  return (
+     <Github
+     className='fixed top-0 right-0 z-[999]'
+    bannerColor="#80b6fa"
+     href="https://github.com/sudongyuer/mini-hulu"
+     />
+  )
+}
+
+export default GithubCorner
