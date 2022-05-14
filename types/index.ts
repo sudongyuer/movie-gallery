e9fc1772806de737005c435bd48ddaf373141c5b
sudongyu <976499226@qq.com>
2022-05-14 20:39:06 +0800

    chore: update index.ts

---
+++ types/index.ts
@@ -0,0 +1,33 @@
+export type result = {
+  "adult": boolean,
+  "backdrop_path": string,
+  "genre_ids":number[],
+  "id": number,
+  "original_language": string,
+  "original_title": string,
+  "overview":string,
+  "popularity": number,
+  "poster_path":string,
+  "release_date":string,
+  "title": string,
+  "video": boolean,
+  "vote_average": number,
+  "vote_count": number
+}
+
+export type resultsArray= {
+  "adult": boolean,
+  "backdrop_path": string,
+  "genre_ids":number[],
+  "id": number,
+  "original_language": string,
+  "original_title": string,
+  "overview":string,
+  "popularity": number,
+  "poster_path":string,
+  "release_date":string,
+  "title": string,
+  "video": boolean,
+  "vote_average": number,
+  "vote_count": number
+}[]
