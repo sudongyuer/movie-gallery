9fa4197442185cc62bf5a01a3b14f6ba794911be
sudongyu <976499226@qq.com>
2022-05-14 20:37:02 +0800

    chore: update HeaderItem.tsx

--- components/HeaderItem.tsx
+++ components/HeaderItem.tsx
@@ -1,7 +1,11 @@
+import { Router, useRouter } from 'next/router'
 import React from 'react'
 function HeaderItem({title,Icon}:{title:string,Icon:(props: React.ComponentProps<'svg'>)=>JSX.Element}) {
+  const router = useRouter()
   return (
-    <div className='flex flex-col items-center w-12 sm:w-20 cursor-pointer  group  hover:text-white'>
+    <div
+    onClick={()=>{router.push(`/other?title=${title}`)}}
+    className='flex flex-col items-center w-12 sm:w-20 cursor-pointer  group  hover:text-white'>
       <Icon className='h-8 mb-1 group-hover:animate-bounce'/>
       <p className='opacity-0  group-hover:opacity-100  tracking-widest'>{title}</p>
     </div>
