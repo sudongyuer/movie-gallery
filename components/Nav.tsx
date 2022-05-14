514ff061fb2a74c54d37a76356b4dd2fc84f9aa5
sudongyu <976499226@qq.com>
2022-05-15 00:46:40 +0800

    chore: update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,7 +9,7 @@ function Nav() {
     <nav className="relative">
       <div className="flex px-10 sm:px-20 text-2xl 
         whitespace-nowrap overflow-scroll scrollbar-hide
-        space-x-10  touch-pan-x
+        space-x-10 overflow-y-hidden
       ">
         {Object.entries(requests).map(([key,{title,url}])=>(
         <h2 key={key} 
