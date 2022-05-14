65d3eedbeba83e69d90a0aa72e4249d24868e29b
sudongyu <976499226@qq.com>
2022-05-14 20:37:22 +0800

    chore: update Results.tsx

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,31 +1,15 @@
 import React from 'react'
 import Thumbnail from './Thumbnail';
 import FlipMove from 'react-flip-move';
-export type resultsArray= {
-  "adult": boolean,
-  "backdrop_path": string,
-  "genre_ids":number[],
-  "id": number,
-  "original_language": string,
-  "original_title": string,
-  "overview":string,
-  "popularity": number,
-  "poster_path":string,
-  "release_date":string,
-  "title": string,
-  "video": boolean,
-  "vote_average": number,
-  "vote_count": number
-}[]
+import { resultsArray } from '../types';
 
 function Results({results}:{results:resultsArray}) {
   console.log(results);
   return (
     <FlipMove
-    staggerDurationBy="100"
     enterAnimation="accordionVertical"
     leaveAnimation="fade"
-    duration={500}
+    duration={300}
     className="px-5 my-10 sm:grid 
     md:grid-cols-2 xl:grid-cols-3 3xl:flex
      justify-center flex-wrap
