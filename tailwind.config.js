f3a0590ad651e1a5345941a3e6f4e1570ad1b8a9
sudongyu <976499226@qq.com>
2022-05-14 20:36:47 +0800

    chore: update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,28 @@ module.exports = {
       screens: {
         "3xl": "2000px",
       },
+      keyframes:{
+        "transform-in":{
+          "from":{
+            transform:"translateX(-1200px)"
+          },
+          "to":{
+            transform: "translateX(0)"
+          }
+        },
+        "fade-in":{
+          "from":{
+            opacity:0
+          },
+          "to":{
+            opacity:1
+          }
+        }
+      },
+      animation:{
+        "transform-in": "transform-in 1.5s ease-in-out",
+        "fade-in": "fade-in 1.5s ease-in-out"
+      }
     },
   },
   plugins: [require('tailwind-scrollbar-hide')],
