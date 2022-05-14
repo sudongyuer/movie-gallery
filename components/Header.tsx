a0f0c842df1dc3b1bee07302e7b82d04abadfb23
sudongyu <976499226@qq.com>
2022-05-14 20:36:54 +0800

    chore: update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,22 +9,23 @@ import {
 import React from 'react'
 import Image from "next/image";
 import HeaderItem from './HeaderItem';
+import HuluIcon from "./HuluIcon";
+import GithubCorner from "./GithubCorner";
 function Header() {
   return (
     <header className="flex flex-col sm:flex-row  m-5 justify-between items-center">
       <div className="flex items-center justify-evenly  max-w-2xl flex-grow">
-      <HeaderItem title={'HOME'} Icon={HomeIcon} />
-      <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
-      <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
-      <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
-      <HeaderItem title="SEARCH" Icon={SearchIcon} />
-      <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+        <HeaderItem title={'HOME'} Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>
-      <Image src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png"
-        width={100}
-        height={100}
-        className="object-contain"
-      />
+     {/* HULU icon */}
+     <HuluIcon/>
+     {/* Github corner */}
+     <GithubCorner/>
     </header>
   )
 }
